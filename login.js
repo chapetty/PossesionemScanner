ce55cbb420580c5fcecf0831d826ad0f9a62a131
Charles Petty <charles.petty@gmail.com>
2019-08-17 21:04:58 -0600

    styling tidied up

--- login.js
+++ login.js
@@ -4,11 +4,19 @@ import { StyleSheet, View, TextInput, Button } from 'react-native';
 const Login = () => {
   return (
     <View>
-        <TextInput placeholder="Username" style={{margin: 10, borderColor: 'black', borderWidth: 1, padding: 5}} />
-        <TextInput placeholder="Password" style={{margin: 10, borderColor: 'black', borderWidth: 1, padding: 5}} textContentType='password' />
-        <Button title="Login" style={{margin:10}} />
+        <TextInput placeholder="Username" style={textBoxStyle} />
+        <TextInput placeholder="Password" style={textBoxStyle} />
+        <Button title="Login" />
     </View>
   );
+};
+
+const textBoxStyle = {
+  margin: 10,
+  padding: 5,
+  borderColor: 'black',
+  borderWidth: 1,
+  borderRadius: 3
 };
 
 export default Login;
