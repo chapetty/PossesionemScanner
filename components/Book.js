defb4ba167f60899c5aaef001e3b3d5d2ff90b3e
Charles Petty <charles.petty@gmail.com>
2019-08-20 18:42:44 -0600

    added basic book information retrieval

---
+++ components/Book.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { ActivityIndicator, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const Book = (props) => {
+
+    const [loading, setLoading] = useState(true);
+
+    const load = () => {
+        fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:' + props.book.isbn)
+            .then(response => response.json())
+            .then(responseJSON => {
+                props.book.title = responseJSON.items[0].volumeInfo.title;
+                props.book.author = responseJSON.items[0].volumeInfo.authors[0];
+                props.book.pages = responseJSON.items[0].volumeInfo.pageCount.toString();
+                props.book.publisher = responseJSON.items[0].volumeInfo.publisher;
+                props.book.publishedDate = responseJSON.items[0].volumeInfo.publishedDate;
+                setLoading(false);
+            });
+    };
+
+    if (!props.book._loaded) {
+        props.book._loaded = true;
+        load();
+    }
+
+    if (loading) {
+        return (
+            <View style={styles.view}>
+                <ActivityIndicator size="small" />
+            </View>
+        );
+    }
+
+    return (
+        <TouchableOpacity>
+            <View style={styles.view}>
+                <Text>Title: {props.book.title}</Text>
+                <Text>Author: {props.book.author}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+
+};
+
+const styles = StyleSheet.create({
+    view: {
+        paddingBottom: 5,
+        paddingTop: 5,
+        borderBottomWidth: 1,
+        borderBottomColor: 'grey',
+        shadowColor: 'red',
+        shadowOffset: { width: 2, height: 2 },
+        shadowOpacity: 100,
+        shadowRadius: 2,
+    }
+});
+
+export default Book;
--- components/BookList.js
+++ components/BookList.js
@@ -2,14 +2,28 @@ import React, { useState } from 'react';
 import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
 
 import AddBook from './AddBook';
+import Book from './Book';
+
+const fakeBookList = [{
+    key: '9780061627019',
+    isbn: '9780061627019',
+    title: 'Basic Writings',
+    author: 'Martin Heidegger'
+}, {
+    key: '9780807064733',
+    isbn: '9780807064733',
+    title: 'The Poetics of Space',
+    author: 'Gaston Bachelard'
+}];
 
 const BookList = (props) => {
 
-    const [bookList, setBookList] = useState([]);
+    const [bookList, setBookList] = useState(fakeBookList);
     const [addBookVisible, setAddBookVisible] = useState(false);
 
     const addBook = (isbn) => {
-        setBookList([...bookList, { key: isbn, value: isbn }]);
+        const newBook = { key: isbn, isbn: isbn };
+        setBookList([...bookList, newBook]);
     };
 
     return (
@@ -19,7 +33,7 @@ const BookList = (props) => {
             <View>
                 {bookList.map(book => {
                     return (
-                        <Text>{book.value}</Text>
+                        <Book book={book} />
                     );
                 })}
             </View>
